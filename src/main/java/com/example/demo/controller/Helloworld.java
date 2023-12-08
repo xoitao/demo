6b5611bd8e4511569ca6bf8b8531db7948262fa7
xoitao <3098504178@qq.com>
2023-12-08 13:56:58 +0800

    master commit2

--- src/main/java/com/example/demo/controller/Helloworld.java
+++ src/main/java/com/example/demo/controller/Helloworld.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Helloworld {
     @RequestMapping("/hello")
     public String helloworld(){
+        System.out.println("master test");
         return "helloworld!!!,hello,git!,hello,git4";
     }
 }
